2d9211b4c417938ca0a966134cf679780bcc2e5d
Gyanesh Jha <57858776+jhagyanesh@users.noreply.github.com>
2020-07-12 22:21:51 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,14 +1,11 @@
 mkdir -p ~/.streamlit/
-
 echo "\
 [general]\n\
 email = \"your-email@domain.com\"\n\
 " > ~/.streamlit/credentials.toml
-
 echo "\
 [server]\n\
 headless = true\n\
 enableCORS=false\n\
-host='0.0.0.0'\n
 port = $PORT\n\
 " > ~/.streamlit/config.toml
